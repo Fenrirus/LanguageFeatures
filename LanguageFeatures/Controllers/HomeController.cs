37c9d4f52f410d7a03e5489879003e0e52d7167d
Marian Marian <fenrir55@gmail.com>
2019-11-03 13:01:53 +0100

    Add project files.

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Przejście do adresu pokazującego przykład";
+        }
+    }
+}
